747b0842aaa4b2d1a17aa25821e86d026b28ff06
Oliver-Cushman <olivermcushman@gmail.com>
2025-11-11 21:33:43 -0800

    fix: async structure

--- bootcamp-milestone-3/src/app/blog/page.tsx
+++ bootcamp-milestone-3/src/app/blog/page.tsx
@@ -2,29 +2,30 @@ import React from "react";
 import BlogPreview from "@/components/blogPreview";
 // import blogs from "@/static/blogData";
 import styles from "./page.module.css";
-import Blog, {BlogData} from "@/database/blogSchema";
+import Blog, { BlogData } from "@/database/blogSchema";
 import connectDB from "@/database/db";
 
-export default function BlogPage() {
-  async function getBlogs() {
-    await connectDB();
-    try {
-      // query for all blogs and sort by date
-      const blogs = await Blog.find().sort({ date: -1 }).orFail();
-      // send a response as the blogs as the message
-      return blogs;
-    } catch (err) {
-      return null;
-    }
+async function getBlogs() {
+  await connectDB();
+  try {
+    // query for all blogs and sort by date
+    const blogs = await Blog.find().sort({ date: -1 }).orFail();
+    // send a response as the blogs as the message
+    return blogs;
+  } catch (err) {
+    return null;
   }
+}
+
+export default async function BlogPage() {
+  const blogs = await getBlogs();
 
   return (
     <div>
       <h1 className={styles.page_title}>Blogs</h1>
       <div className={styles.blog_container}>
-        {getBlogs().then((blogs) => {
-          if (blogs) {
-            return blogs.map((blog) => {
+        {blogs
+          ? blogs.map((blog) => {
               let props: BlogData = {
                 title: blog.title,
                 date: blog.date,
@@ -32,13 +33,11 @@ export default function BlogPage() {
                 image: blog.image,
                 imageAlt: blog.imageAlt,
                 slug: blog.slug,
-                id: blog.id
-              }
-              return <BlogPreview key={blog.id} {...props}  />;
-            });
-          }
-          return null;
-        })}
+                id: blog.id,
+              };
+              return <BlogPreview key={blog.id} {...props} />;
+            })
+          : null}
       </div>
     </div>
   );
